68e61be7f709aa6bc9efa2aa8a3e86c75be28bb1
Gad Abraham <gad@supernook.(none)>
2010-08-24 23:04:30 +1000

    Cleanup

--- src/runplink.sh
+++ src/runplink.sh
@@ -1,18 +1,27 @@
 PLINK="p-link"
 
-DIR_STEM="sim6."
+DIR_STEM="sim7."
 HAPMAP="$PWD/HapMap/genotypes_chr1_JPT+CHB_r22_nr.b36_fwd_legend.txt.map"
 
 results="results"
-for i in $(seq 1 10);
+for i in $(seq 31 31);
 do
    dir="$DIR_STEM""$i"
+   while ! [[ -d "$dir" && -a "$dir/sim.bed" ]];
+   do
+      echo "Dir $dir doesn't exist yet, sleeping..."
+      sleep 660
+   done
+
    dir_res="$dir/$results"
    if ! [ -d "$dir_res" ]; then
       mkdir "$dir_res"
    fi
    pushd "$dir/$results"
-   $PLINK --ped ../sim.ped --map $HAPMAP --logistic
+   if ! [ -a "plink.assoc.logistic" ];
+   then
+      $PLINK --bfile ../sim --logistic
+   fi
    popd
 done
 
