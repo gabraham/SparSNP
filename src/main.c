d10471ec370bc3b04b7e10e08af594e91a976f75
Gad Abraham <gabraham@csse.unimelb.edu.au>
2010-12-15 15:04:59 +1100

    Forgot to remove debugging code

--- src/main.c
+++ src/main.c
@@ -132,11 +132,7 @@ int run_predict_beta(gmatrix *g, predict predict_func,
    {
       g->nextcol(g, &sm, j, NA_ACTION_ZERO);
       for(i = 0 ; i < n ; i++)
-      {
 	 lp[i] += sm.x[i] * beta[j];
-	 printf("%d %d\n", i, j);
-	 fflush(stdout);
-      }
    }
    
    for(i = 0 ; i < n ; i++)
