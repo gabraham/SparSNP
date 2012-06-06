c2b495328a0542176b0d7ffc7735201204c720ff
Gad Abraham <gad.abraham@gmail.com>
2012-06-06 16:49:19 +1000

    Added ability to use GNU parallel in crossval

---
+++ src/cv10.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ] || [ -z "$2" ];
+then
+   echo "Usage: cv.sh <root name of PLINK file> <model>"
+   echo "where model is one of: linear, sqrhinge"
+   exit 1
+fi
+
+# For GNU parallel
+[[ -z "$NUMPROCS" ]] && NUMPROCS=2
+
+D=$(dirname $1)
+F=$(basename $1)
+ROOT=$D/$F
+
+EXEDIR=~/Code/cd/src/sparsnp
+
+######################################################################
+# User modifiable parameters
+
+# number of cross-validation folds
+[[ -z "$NFOLDS" ]] && NFOLDS=10
+
+# number of cross-validation replications
+[[ -z "$NREPS" ]] && NREPS=10
+
+# number of penalties to look at
+[[ -z "$NLAMBDA1" ]] && NLAMBDA1=30
+
+[[ -z "$L1MIN" ]] && L1MIN=0.001
+
+[[ -z "$LAMBDA2" ]] && LAMBDA2=0
+
+MODEL=$2
+
+######################################################################
+
+######################################################################
+# Don't change these unless you know what you're doing
+N=$(cat "$ROOT".fam | wc -l)
+P=$(cat "$ROOT".bim | wc -l)
+BED=$($EXEDIR/realpath "$ROOT".bed)
+FAM=$($EXEDIR/realpath "$ROOT".fam)
+BIM=$($EXEDIR/realpath "$ROOT".bim)
+SCALE=scale.bin
+FOLDIND="-foldind folds.ind"
+######################################################################
+
+# maximum number of non-zero SNPs to consider in model
+if [ -z "$NZMAX" ];
+then
+   if [ $N -lt $P ];
+   then
+      NZMAX=$N
+   else
+      NZMAX=$P
+   fi
+fi
+
+echo "BED: $BED"
+echo "BIM: $BIM"
+echo "FAM: $FAM"
+
+DIR=discovery
+
+if ! [ -d "$DIR" ];
+then
+   mkdir $DIR
+fi
+pushd $DIR
+
+cat >params.txt<<EOF
+ROOT=$ROOT
+NFOLDS=$NFOLDS
+NREPS=$NREPS
+NZMAX=$NZMAX
+NLAMBDA1=$NLAMBDA1
+LAMBDA2=$LAMBDA2
+MODEL=$MODEL
+EOF
+
+awk '{print $2, $5}' "$BIM" > snps.txt
+
+if [ $NFOLDS -le 1 ];
+then
+   FOLDIND=""
+fi
+
+[[ -z "$REP_START" ]] && REP_START=1
+[[ -z "$REP_END" ]] && REP_END=$NREPS
+
+echo "REP_START: $REP_START"
+echo "REP_END: $REP_END"
+
+#for((i=$REP_START;i<=$REP_END;i++))
+#do
+function run {
+   i=$1
+   dir="crossval$i"
+
+   echo "dir=$dir"
+
+   # don't clobber an existing directory
+   if ! [ -d $dir ];
+   then
+      mkdir $dir
+      pushd $dir
+
+      # Create cross-validation folds
+      $EXEDIR/split -folds folds.txt -ind folds.ind -nfolds $NFOLDS -n $N
+
+      # Get scale of each crossval fold
+      $EXEDIR/scale -bin $BED -n $N -p $P $FOLDIND 
+   
+      # Run the model
+      $EXEDIR/cd -train -model $MODEL -n $N -p $P \
+	 -scale $SCALE -bin $BED -nzmax $NZMAX -nl1 $NLAMBDA1 -l1min $L1MIN -v \
+	 $FOLDIND -fam $FAM -l2 $LAMBDA2
+ 
+      # Predict for test folds
+      B=$(for((i=0;i<NLAMBDA1;i++)); do printf 'beta.csv.%02d ' $i; done)
+      $EXEDIR/cd -predict -model $MODEL -n $N -p $P -v \
+	 -bin $BED -betafiles $B \
+	 -scale $SCALE \
+	 $FOLDIND -fam $FAM
+
+      popd
+   else
+      echo "skipping $dir"
+   fi
+#done
+}
+
+export -f run
+export EXEDIR NFOLDS N P BED FAM FOLDIND MODEL SCALE NZMAX NLAMBDA1 L1MIN LAMBDA2
+
+if [ $DOPARALLEL ]
+then
+   seq $REP_START $REP_END | parallel -j$NUMPROCS -I{} run {}
+else
+   for((i=$REP_START;i<=$REP_END;i++)); do run $i; done
+fi
+
+popd
+
---
+++ src/getmodels.R
@@ -0,0 +1,139 @@
+#!/usr/bin/env Rscript
+
+# getmodels.R:
+# Averages models along the regularisation path, across 
+# cross-validation replications, and outputs these models
+# plus the best model, in the form of a PLINK score file
+# 
+
+# Note: we assume that the solution path produces roughly the same number
+# of SNPs in the model across all cross-validation replications. When sample
+# sizes are small, or the signal is very weak, this may not hold.
+#
+# Also, if you ask for a model size that is far from the model sizes that have
+# been run, then you won't get back a prediction based on that model size,
+# instead you'll get a prediction from a the closest model that could be
+# found.
+
+DIR="discovery"
+AVGFILE="avg_weights_opt"
+
+usage <- "usage: getmodels.R [nzreq=<num SNPS required>]"
+
+args <- commandArgs(TRUE)
+s <- strsplit(args, "=")
+for(m in s) {
+   eval(parse(text=sprintf("%s=\"%s\"", m[1], m[2])))
+}
+
+if(!exists("nzreq", mode="character")) {
+   stop(usage)
+}
+
+nzreq <- as.numeric(nzreq)
+if(is.na(nzreq) || as.integer(nzreq) != nzreq || nzreq <= 0) {
+   stop(usage)
+}
+
+# Parse the parameters
+param <- scan("discovery/params.txt", what=character())
+s <- sapply(param, strsplit, "=")
+for(i in seq(along=s)) {
+   s1 <- s[[i]][1]
+   s2 <- s[[i]][2]
+   if(is.na(as.numeric(s2))) {
+      s2 <- paste("\"", s2, "\"", sep="")
+   }
+   x <- paste(s1, "=", s2)
+   eval(parse(text=x))
+}
+
+nreps <- NREPS
+nfolds <- NFOLDS
+dir <- DIR
+avgfile <- AVGFILE
+
+r <- read.table(sprintf("%s/snps.txt", dir), header=FALSE,
+   stringsAsFactors=FALSE)
+
+nz <- lapply(1:nreps, function(i) {
+   lapply(1:nfolds, function(j) {
+      scan(sprintf("%s/crossval%s/nonzero.csv.%02d", dir, i, j - 1),
+	 quiet=TRUE)
+   })
+})
+
+# Get best model with the closest model size to requested
+nzopt <- lapply(nz, sapply, function(n) which.min((n - nzreq)^2))
+
+b <- lapply(1:nreps, function(rep) {
+   lapply(1:nfolds, function(fold) {
+      w <- nzopt[[rep]][fold]
+      f <- sprintf("%s/crossval%s/beta.csv.%02d.%02d",
+	 dir, rep, w - 1, fold - 1)
+      read.table(f, sep=":", header=FALSE)
+   })
+})
+
+b <- unlist(b, recursive=FALSE)
+b2 <- do.call(rbind, b)
+b2m <- with(b2, tapply(V2, V1, function(x) sum(x) / length(b)))
+
+# Evaluate the entire path of solutions, averaging over all folds and
+# replications and clusters of solutions with roughly same size.
+# We don't check cluster size, just take the kth cluster across all
+# replications, as with a fine enough penalty grid model sizes will be
+# similar.
+bpath <- lapply(1:nreps, function(rep) {
+   lapply(1:nfolds, function(fold) {
+      l <- list.files(
+	 path=sprintf("%s/crossval%s", dir, rep),
+	 pattern=sprintf("^beta.csv.[[:digit:]]+.%02d$", fold - 1),
+	 full.names=TRUE
+      )
+      lapply(l, function(x) {
+	 r <- read.table(x, sep=":", header=FALSE)
+	 r$NonZero <- nrow(r)
+	 r
+      })
+   })
+})
+bpath2 <- unlist(bpath, recursive=FALSE)
+m <- min(sapply(bpath2, length))
+bpath3 <- lapply(bpath2, head, n=m)
+bpathm <- lapply(1:m, function(i) {
+   r <- lapply(bpath3, function(x) x[[i]])
+   r2 <- do.call(rbind, r) 
+   with(r2, tapply(V2, V1, function(x) sum(x) / length(r)))
+})
+
+# Average weights at chosen model, over cross-validation reps, 
+# including intercept
+write.table(b2m["0"], file=sprintf("%s/intercept.txt", dir),
+   col.names=FALSE, sep=",", quote=FALSE, row.names=FALSE)
+
+# PLINK score file, excluding intercept
+b2mp <- b2m[grep("^0$", names(b2m), invert=TRUE)]
+b2mp <- b2mp[order(abs(b2mp), decreasing=TRUE)][1:nzreq]
+snps <- read.table(sprintf("%s/snps.txt", dir))
+w <- as.integer(names(b2mp))
+
+d <- data.frame(snps[w,], b2mp)
+
+write.table(d, file=sprintf("%s/%s.score", dir, avgfile),
+   quote=FALSE, row.names=FALSE, col.names=FALSE)
+
+# Write averaged models over the regularisation path, plus intercept for each
+# model
+for(i in seq(along=bpathm)) {
+   x <- bpathm[[i]]
+   x <- x[order(abs(x), decreasing=TRUE)]
+   w <- as.integer(names(x))
+   d <- data.frame(snps[w[w != 0], ], x[w != 0])
+   write.table(d, file=sprintf("%s/%s_path_%s.score", dir, avgfile, i),
+      quote=FALSE, row.names=FALSE, col.names=FALSE)
+   write.table(x["0"], file=sprintf("%s/intercept_path_%s.txt", dir, i),
+      col.names=FALSE, sep=",", quote=FALSE, row.names=FALSE)
+}
+
+
