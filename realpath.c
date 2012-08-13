e126184ed393c71c43a6f222c9eed519989ee5ae
Gad Abraham <gad.abraham@gmail.com>
2012-08-13 15:47:06 +1000

    Cleaned up realpath to fail gracefully on bad input

--- realpath.c
+++ realpath.c
@@ -2,15 +2,30 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>    
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/param.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    char *symlinkpath = argv[1];
-    char actualpath [PATH_MAX];
-    char *ptr;
-    ptr = realpath(symlinkpath, actualpath);
-    printf("%s\n", ptr);
-    return EXIT_SUCCESS;
+   char *symlinkpath = NULL;
+   char actualpath [PATH_MAX];
+   char *ptr;
+
+   if(argc > 1)
+   {
+      symlinkpath = argv[1];
+      if((ptr = realpath(symlinkpath, actualpath)))
+      {
+	 printf("%s\n", ptr);
+	 return EXIT_SUCCESS;
+      }
+      fprintf(stderr, "realpath: %s\n", strerror(errno));
+   }
+   else
+      fprintf(stderr, "realpath: missing file name\n");
+   
+   return EXIT_FAILURE;
 }
 
